1afd84ff346e0df0155c22a409202c009e3bbda8
AzureDuulga <azure.duulga.enkhtur@gmail.com>
2023-01-27 17:05:24 +0800

    angid hiiv

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -18,6 +18,8 @@ const Banner = () => {
           sx={{
             backgroundImage:
               "url(https://images.unsplash.com/photo-1578894381163-e72c17f2d45f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dHJhdmVsJTIwbWFwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
             width: "1127px",
             height: "395px",
             backgroundColor: "black",
--- src/pages/Auth/SignIn/index.jsx
+++ src/pages/Auth/SignIn/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
@@ -10,97 +10,121 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import { Snackbar, Alert } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default class SignIn extends Component {
-  constructor(props) {
-    super(props);
-  }
-  handleSubmit = (event) => {
-    event.preventDefault();
-    // const data = new FormData(event.currentTarget);
-    // console.log({
-    //   email: data.get("email"),
-    //   password: data.get("password"),
-    // });
+const SignIn = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  const onClose = () => {
+    setOpen(false);
   };
-  render() {
-    return (
-      <div>
-        <Container component="main" maxWidth="xs">
-          <Box
-            sx={{
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box
-              component="form"
-              onSubmit={this.handleSubmit}
-              noValidate
-              sx={{ mt: 1 }}
+  const login = () => {
+    console.log("hsdfghdfg");
+    if (email === "" || password === "") {
+      setOpen(true);
+    } else {
+      console.log(email + password);
+      localStorage.setItem("isLogged", true);
+      navigate("/");
+    }
+  };
+  const changeEmail = (e) => {
+    setEmail(e.target.value);
+    console.log("Email: ", e);
+  };
+  const changePassword = (e) => {
+    setPassword(e.target.value);
+    console.log("Email: ", e.target.value);
+  };
+
+  return (
+    <div>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={changeEmail}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={changePassword}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              onClick={login}
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
             >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Button
-                    onClick={() => {
-                      this.props.setIsSignIn(false);
-                    }}
-                    variant="text"
-                  >
-                    Бүртгүүлэх
-                  </Button>
-                </Grid>
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
               </Grid>
-            </Box>
+              <Grid item>
+                <Button
+                  onClick={() => {
+                    props.setIsSignIn(false);
+                  }}
+                  variant="text"
+                >
+                  Бүртгүүлэх
+                </Button>
+              </Grid>
+            </Grid>
           </Box>
-          {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
-        </Container>
-      </div>
-    );
-  }
-}
+        </Box>
+        <Snackbar
+          open={open}
+          autoHideDuration={3000}
+          onClose={onClose}
+          message={"hi"}
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        >
+          <Alert severity="error">Утга оруулна уу</Alert>
+        </Snackbar>
+      </Container>
+    </div>
+  );
+};
+
+export default SignIn;
