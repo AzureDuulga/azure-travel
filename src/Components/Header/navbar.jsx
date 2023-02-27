7c6d5ce5a3a58b0dc908c4fad55cecd53ed44d50
DuulgaEnkhtur <duulga16@gmail.com>
2023-02-27 15:08:32 +0800

    pakcage changes

--- src/Components/Header/navbar.jsx
+++ src/Components/Header/navbar.jsx
@@ -11,8 +11,6 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-
 const pages = ["Home", "Stays", "Flights", "Packages"];
 const settings = ["Home", "Stays", "Flights", "Packages"];
 
